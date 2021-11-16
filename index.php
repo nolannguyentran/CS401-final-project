4f89aadd0c020c7187626ccbf7a4e048033036a6
BoiseState Student <student@u.boisestate.edu>
2021-11-15 17:19:23 -0700

    Changed given files

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include("header.html"); 
 
-<head>
-  <link rel="stylesheet" href="css/style.css">
-  <title>Hello World</title>
-</head>
-
-
-<body>  
-  <?php echo "<h1>Hello World! (CSS has changed this text to blue!)</h1>" ?>  
-  <img src="img/thedude.jpg" alt="The dude">
-  <?php include("example.php"); ?>
-  <button type="button" onclick="helloJS()">Click Me!</button>
-
-  <h1>This is a change </h1>
   
-</body>
-  <script src="js/main.js"></script>
-</html>
+  
+//<script src="js/main.js"></script>
+?> 
